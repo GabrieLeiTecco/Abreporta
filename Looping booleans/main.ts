80018ce2926709c8c62a29c4e45315c5686d0b16
Gabriel Leite Tecco <111099300+GabrieLeiTecco@users.noreply.github.com>
2023-12-06 10:14:10 -0300

    Add files via upload

---
+++ Looping booleans/main.ts
@@ -0,0 +1,47 @@
+// o que eu preciso fazer?
+// iniciar com a porta fechada
+// receber a resposta do usuario
+// verificar se a porta está fechada ou aberta
+// criar um looping para perguntar novamente
+
+function passarPorta(passou : boolean){
+    if (passou == true){
+        verificarPorta()
+    }else{
+        ficarParado(passou)
+    }
+}
+
+function verificarPorta(){
+    let passou : boolean = false
+    let resp : string = String(prompt("Você encontra uma porta fechada na sua frente \nVocê deseja passar pela porta? "))
+
+    if (resp == "sim" || resp == "Sim" || resp == "s"){
+        passou = true
+        passarPorta(passou)
+    }else if(resp == "nao" || resp == "não" || resp == "Não" || resp == "Nao"){
+        passou = false
+        passarPorta(passou)
+    }else if(resp == "cancelar" || resp == "cancel"){
+        console.log("cabou")
+    }else{
+        verificarPorta()
+    }
+}
+
+function ficarParado(passou : boolean){
+    let resp : string = String(prompt("Você fica parado com uma porta fechada na sua frente \nVocê deseja passar pela porta? "))
+
+    if (resp == "sim" || resp == "Sim" || resp == "s"){
+        passou = true
+        passarPorta(true)
+    }else if(resp == "nao" || resp == "não" || resp == "Não" || resp == "Nao"){
+        passou = false
+        passarPorta(false)
+    }else if(resp == "cancelar" || resp == "cancel"){
+        console.log("cabou")
+    }else{
+        verificarPorta()
+    }
+}
+verificarPorta()
